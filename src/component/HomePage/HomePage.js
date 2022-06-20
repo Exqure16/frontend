f29208be3ecb2f4068fff603766d7b0c8a51d200
davidessien925 <davidessien925@gmail.com>
2022-06-20 12:21:57 +0100

    desktop view complete for now

--- src/component/HomePage/HomePage.js
+++ src/component/HomePage/HomePage.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Cards from './cards'
 import SingleCard from './singleCard'
 import './HomePageStyles.css';
+import ExqureLogo from '../images/logo.png';
+import facebook from '../images/facebook.png';
+import instagram from '../images/instagram.png';
+import twitter from '../images/twitter.png';
+import linkedIn from '../images/linkdln.png';
 
 export default function HomePage() {
   return (
@@ -19,7 +24,7 @@ export default function HomePage() {
                 </p>
                 <button>Get Started</button>
             </div>
-            <div className="writeup-image"><img alt="side_image"/></div>
+            <div className="writeup-image"><img alt="side-pic"/></div>
         </section>
         
         <section className="section2">
@@ -29,15 +34,59 @@ export default function HomePage() {
         </section>
 
         <section className="section3">
-            <h3>Experience 100% safety in Buying and Selling goods and service</h3>
-            <div className="card">
-                <SingleCard />
-                <SingleCard />
-                <SingleCard />
-            </div>
-            
+            <h3>Experience 100% safety in Buying and Selling goods and services</h3>
+            <SingleCard />
             <button>Get Started</button>
         </section>
+
+        <section className="section4">
+            <h2>Always get Safe transactions with Exqure</h2>
+            <div className='row-div'>
+                <div className='exqureLogo'>
+                    <img alt="exqure-logo" src={ExqureLogo}/>
+                </div>
+                <button>Privacy Policy</button>
+                <button>Terms of Use</button>
+                <div className="socials">
+                    <div className='social-media'>
+                        <img alt="social-media" src={instagram}/>
+                    </div>
+                    <div className='social-media'>
+                        <img alt="social-media" src={twitter}/>
+                    </div>
+                    <div className='social-media'>
+                        <img alt="social-media" src={linkedIn}/>
+                    </div>
+                    <div className='social-media'>
+                        <img alt="social-media" src={facebook}/>
+                    </div>
+                </div>
+            </div>
+            <div className='row-div'>
+                <h3>Exqure Services</h3>
+                <button>Fashion</button>
+                <button>Furniture</button>
+                <button>Vehicles</button>
+                <button>Electronics</button>
+            </div>
+            <div className='row-div'>
+                <h3>Support</h3>
+                <button>Fraud Prevention</button>
+                <button>Security</button>
+                <button>Payment Options</button>
+                <button>FAQs</button>
+            </div>
+            <div className='row-div'>
+                <h3>Company</h3>
+                <button>About us</button>
+                <button>Contacts</button>
+                <button>Blog</button>
+                <button>Join our team</button>
+            </div>
+        </section>
+        <footer>
+            Copyright  2022, Exqure, All rights reserved.
+        </footer>
     </div>
   )
 }
--- src/component/HomePage/singleCard.jsx
+++ src/component/HomePage/singleCard.jsx
@@ -2,21 +2,36 @@ import React from 'react';
 
 export default function SingleCard() {
   const cardArray = [
-    {text: 'Login/Sign up to Exqure', image: `${'placeholder'}`},
+    {
+      text: 'Electronics Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci egestas egestas dignissim odio pellentesque massa nunc tortor gravida.',
+      title: 'Electronics',
+      image: `${'placeholder'}`
+    },    
+    {
+      text: 'Electronics Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci egestas egestas dignissim odio pellentesque massa nunc tortor gravida.',
+      title: 'Electronics',
+      image: `${'placeholder'}`
+    },
+    {
+      text: 'Electronics Lorem ipsum dolor sit amet, consectetur adipiscing elit. Orci egestas egestas dignissim odio pellentesque massa nunc tortor gravida.',
+      title: 'Electronics',
+      image: `${'placeholder'}`
+    },
   ]
 
   const CardMap = cardArray.map((card, i) => (
     <div className="cardbody" key={i}>
       <div className="img-container">
-        <img className="card-img" alt="description image" src={card.image}/>
+        <img className="card-img" alt="description-pic" src={card.image}/>
       </div>
+      <div className="card-title">{card.title}</div>
       <div className="card-text">{card.text}</div>
     </div>
   ))
 
   return (
-    <div>
-        {CardMap}
+    <div className="card">
+      {CardMap}
     </div>
   )
 }
