f1f49c7a3b32ce72ba6467486815f4294226f226
Reginald <dono4executive@gmail.com>
2022-06-25 22:25:23 -0700

    first commit of navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 
 import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Comingsoon from './component/Comingsoon/Comingsoon';
 import Login from './component/LoginPage/Login';
 import HomePage from './component/HomePage/HomePage';
+import Header from './component/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route path='exqure-frontend/' element={<Comingsoon />} />
         <Route path='exqure-frontend/login' element={<Login />} />
