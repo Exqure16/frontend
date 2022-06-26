afaaf9dc7fe54d327d2e2ce0f09ae9be2afd59d1
davidessien925 <davidessien925@gmail.com>
2022-06-26 09:24:21 +0100

    homepage update 2

--- src/component/HomePage/HomePage.js
+++ src/component/HomePage/HomePage.js
@@ -14,6 +14,24 @@ import { faVideo } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faFacebookSquare, faTwitterSquare, faInstagramSquare, faLinkedin, faVideo)
 
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+}
+  
+window.addEventListener("scroll", reveal);
+
 export default function HomePage() {
   return (
     <div className="homepage-main">
