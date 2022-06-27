bd23a69ff780aed3a6ffa5626b90d22bda1a5546
davidessien925 <davidessien925@gmail.com>
2022-06-27 14:11:33 +0100

    footer changed

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Comingsoon from './component/Comingsoon/Comingsoon';
+import Header from './component/Header/Header';
 import Login from './component/LoginPage/Login';
 import HomePage from './component/HomePage/HomePage';
 import Footer from './component/Footer'
@@ -11,6 +12,7 @@ import Footer from './component/Footer'
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
       <Route path='exqure-frontend/' element={<Comingsoon/>} />
       <Route path='exqure-frontend/login' element={<Login/>} />
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
+import { faFacebookSquare, faTwitterSquare, faInstagramSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import ExqureLogo2 from './images/logo2.svg';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+library.add(faFacebookSquare, faTwitterSquare, faInstagramSquare, faLinkedin)
+
 
 export default function Footer() {
   return (
+    <div>
+      <section className="section4">
+        <h2>Always get safe transactions with Exqure</h2>
+        <div className='row-div logodiv'>
+            <div className='exqureLogo'>
+                <img alt="exqure-logo" src={ExqureLogo2}/>
+            </div>
+            <button>Privacy Policy</button>
+            <button>Terms of Use</button>
+            <div className="socials">
+                <FontAwesomeIcon icon="fa-brands fa-facebook-square" size="2x" />
+                <FontAwesomeIcon icon="fa-brands fa-twitter-square" size="2x"/>
+                <FontAwesomeIcon icon="fa-brands fa-instagram-square" size="2x"/>
+                <FontAwesomeIcon icon="fa-brands fa-linkedin" size="2x" />
+            </div>
+        </div>
+        <div className='row-div'>
+            <h3>Exqure Services</h3>
+            <button>Fashion</button>
+            <button>Furniture</button>
+            <button>Cryptocurrency</button>
+            <button>Others</button>
+        </div>
+        <div className='row-div'>
+            <h3>Support</h3>
+            <button>Fraud Prevention</button>
+            <button>Security</button>
+            <button>Payment Options</button>
+            <button>FAQs</button>
+        </div>
+        <div className='row-div'>
+            <h3>Company</h3>
+            <button>About us</button>
+            <button>Contacts</button>
+            <button>Blog</button>
+            <button>Join our team</button>
+        </div>
+    </section>
     <footer>
         Copyright  2022, Exqure, All rights reserved.
     </footer>
+    </div>
+    
   )
 }
