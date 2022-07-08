cd166a1bde36d04352d6917ac12e84667a528ae0
Mathias Akari <cyphercode59@gmail.com>
2022-07-08 13:42:47 -0700

    Created notification page(empty and full)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './component/Header/Header';
-import Login from './component/LoginPage/Login';
-import HomePage from './component/HomePage/HomePage';
-import Footer from './component/Footer';
-import Signup from './component/Signup/Signup';
-import ForgotPassword from './component/LoginPage/ForgotPassword';
-import ResetPassword from './component/LoginPage/ResetPassword';
+// import Header from './component/Header/Header';
+// import Login from './component/LoginPage/Login';
+// import HomePage from './component/HomePage/HomePage';
+// import Footer from './component/Footer';
+// import Signup from './component/Signup/Signup';
+// import ForgotPassword from './component/LoginPage/ForgotPassword';
+// import ResetPassword from './component/LoginPage/ResetPassword';
+import Notifications from './component/notifications/Notifications';
 
 function App() {
   return (
     <div>
-      <Header />
-      <Routes>
+      {/* <Header /> */}
+      {/* <Routes>
         <Route path='frontend/' element={<HomePage />} />
         <Route path='frontend/login' element={<Login />} />
         <Route path='frontend/forgot' element={<ForgotPassword />} />
         <Route path='frontend/reset' element={<ResetPassword />} />
         <Route path='frontend/signup' element={<Signup />} />
-      </Routes>
-      <Footer />
+      </Routes> */}
+      <Notifications />
+      {/* <Footer /> */}
     </div>
   );
 }
