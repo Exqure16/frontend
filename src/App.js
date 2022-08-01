0d932be71fd98facea96b919262aa428256e59f7
Reginald <dono4executive@gmail.com>
2022-08-01 20:18:20 +0100

    payment method

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './component/Header/Header';
 import Login from './component/LoginPage/Login';
 import HomePage from './component/HomePage/HomePage';
-import Footer from './component/Footer';
-import Signup from './component/Signup/Signup';
+// import Footer from './component/Footer';
+// import Signup from './component/Signup/Signup';
 import ForgotPassword from './component/LoginPage/ForgotPassword';
 import ResetPassword from './component/LoginPage/ResetPassword';
 
@@ -13,34 +13,33 @@ import CreateTransaction from './component/TransactionPages/CreateTransaction/Cr
 
 import TransBuyer from './component/TransactionPages/MyTransaction/MyTransaction_Buyer';
 import TransBuyerCrypto from './component/TransactionPages/MyTransaction/MyTransaction_Buyer-Crypto';
-
+import GeneralSettings from './component/GeneralSettings/GeneralSettings';
 import Personal from './component/Profile/Personal';
 import NoTransactions from './component/TransactionPages/NoTransaction';
 import WithTransaction from './component/TransactionPages/WithTransaction';
 import TransSeller from './component/TransactionPages/Transaction/TransSeller';
-import TransBuyer from './component/TransactionPages/Transaction/TransBuyer';
+// import TransBuyer from './component/TransactionPages/Transaction/TransBuyer';
 import Notifications from './component/notifications/Notifications';
 import TransactionAccepted from './component/TransactionPages/TransactionAccepted';
-import PsWithTransaction from './component/PaymentSettings/PsWithTransaction';
+// import PsWithTransaction from './component/PaymentSettings/PsWithTransaction';
 import PaymentSettings from './component/PaymentSettings/PaymentSettings';
 import TransactionAccepteditems from './component/TransactionPages/TransactionAccepteditems';
 import TransactionDeclined from './component/TransactionPages/TransactionDeclined';
 import Accountsetting from './component/Accountsetting/Accountsetting';
+import PaymentMethod from './component/PaymentMethod/PaymentMethod';
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
-      <Blog />
-      <GeneralSettings />
-      <TransactionSeller />
-       <Routes>
+      <Header />
+      {/* <Blog />
+      <TransactionSeller /> */}
+      <Routes>
         <Route path='/frontend' element={<HomePage />} />
         <Route path='frontend/login' element={<Login />} />
         <Route path='frontend/forgot' element={<ForgotPassword />} />
         <Route path='frontend/reset' element={<ResetPassword />} />
-        <Route path='frontend/signup' element={<Signup />} />
-
+        {/* <Route path='frontend/signup' element={<Signup />} /> */}
         <Route path='frontend/create' element={<CreateTransaction />} />
         <Route
           path='frontend/acceptedterms'
@@ -51,21 +50,25 @@ function App() {
           element={<TransactionAccepteditems />}
         />
         <Route path='frontend/declined' element={<TransactionDeclined />} />
-
+        <Route path='frontend/paymethod' element={<PaymentMethod />} />
         <Route path='frontend/transaction' element={<NoTransactions />} />
         <Route path='frontend/transactions' element={<WithTransaction />} />
         <Route path='frontend/transbuyer' element={<TransBuyer />} />
         <Route path='frontend/transSeller' element={<TransSeller />} />
         <Route path='frontend/notifications' element={<Notifications />} />
 
-        <Route
-          path='frontend/psWithTransaction'
-          element={<PsWithTransaction />}
-        />
+        <Route path='frontend/accountsetting' element={<Accountsetting />}>
+          <Route path='general' element={<GeneralSettings />} />
+          <Route path='profile' element={<Personal />} />
+          <Route path='paymentsetting' element={<PaymentSettings />} />
+        </Route>
         <Route path='frontend/mytransactions' element={<TransBuyer />} />
-        <Route path='frontend/mytransactions-crypto' element={<TransBuyerCrypto />} />
+        <Route
+          path='frontend/mytransactions-crypto'
+          element={<TransBuyerCrypto />}
+        />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
